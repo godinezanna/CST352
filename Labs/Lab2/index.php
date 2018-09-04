ebc39f2c1a1d1c8405a1dcf7173ec7e7f750ab8a
Anna Godinez <angodinez@csumb.edu>
2018-09-04 19:53:43 +0000

    Adding first index.php

---
+++ Labs/Lab2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+        
+        <?php
+        
+        echo  "<img src='image/symbol.php"
+
+    </body>
+</html>
