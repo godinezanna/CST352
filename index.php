5f1bf3f31334b70c79e6d84ca67f0c63abc52725
Anna Godinez <angodinez@csumb.edu>
2018-09-05 22:58:43 +0000

    Adding index.php

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo "Hello World Anna Godinez's website";
+echo "Anna Godinez CST-352";
 
 ?>
-
+<br/>
+<br/>
+<p>Click on links</p>
 <a href="https://preview.c9users.io/angodinez/anna-godinez-cst352/cst352/Labs/Lab1/index.html?_c9_id=livepreview5&_c9_host=https://ide.c9.io">Lab 1</a>
 </body>
 </html>
