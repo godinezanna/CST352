a505eebbacd7722286c330dbd821a1c8ca7f5f1a
Anna Godinez <angodinez@csumb.edu>
2018-11-19 08:24:22 +0000

    adding all files

--- Labs/Lab7/guessNumber/js/functions.js
+++ Labs/Lab7/guessNumber/js/functions.js
@@ -1,11 +1,11 @@
 //JavaScript goes here!!!
-var randomNumber = Math.floor(Math.random()*99) + 1;
+var randomNumber = Math.floor(Math.random() * 99) + 1;
 var guesses = document.querySelector ('#guesses');
 var lastResult = document.querySelector ('#lastResult');
 var lowOrHi = document.querySelector ('#lowOrHi');
 
 var guessSubmit = document.querySelector ('.guessSubmit');
-var guesseField = document.querySelector ('.guesseField');
+var guessField = document.querySelector ('.guessField');
 
 var guessCount = 1;
 var resetButton = document.querySelector ('#reset');
@@ -24,7 +24,7 @@ function checkGuess(){
     guesses.innerHTML += userGuess + ' ';
 
         if (userGuess === randomNumber) {
-            lastResult.innerHTML = 'Congrats! You got it right!';
+            lastResult.innerHTML = 'Congratulations! You got it right!';
             lastResult.style.backgroundColor = "green";
             lowOrHi.innerHTML = '';
             setGameOver();
@@ -36,10 +36,10 @@ function checkGuess(){
             lastResult.style.backgroundColor = "red";
             if (userGuess < randomNumber) {
                 lowOrHi.innerHTML = 'Last guess was too low!';
-            } else if (userGuess > randomNumber) {
+            } else if(userGuess > randomNumber) {
                 lowOrHi.innerHTML = 'Last guess was too high!';
             }
-    }
+        }
     
     guessCount++;
     guessField.value = '';
@@ -47,12 +47,14 @@ function checkGuess(){
 }
 
 guessSubmit.addEventListener('click',checkGuess);
+
 function setGameOver(){
     guessField.disabled = true;
     guessSubmit.disabled = true;
     resetButton.style.display = 'inline';
     resetButton.addEventListener('click', resetGame);
 }
+
 function resetGame(){
     guessCount = 1;
     
@@ -60,6 +62,7 @@ function resetGame(){
     for(var i = 0; i < resetParas.length; i++){
         resetParas[i].textContent = '';
     }
+    
     resetButton.style.display = 'none';
     
     guessField.disabled = false;
@@ -69,5 +72,5 @@ function resetGame(){
     
     lastResult.style.backgroundColor = 'white';
     
-    randomNumber = Math.floor(Math.random()*99) + 1;
+    randomNumber = Math.floor(Math.random() * 99) + 1;
 }
